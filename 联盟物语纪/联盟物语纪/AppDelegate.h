6281fee7fdea004baaf8b0203b4fd988aba6f3ed
赵博 <zhaobo@zhaobodeMacBook-Pro.local>
2016-07-28 16:32:18 +0800

    创建工程

---
+++ 联盟物语纪/联盟物语纪/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  联盟物语纪
+//
+//  Created by 赵博 on 16/7/28.
+//  Copyright © 2016年 赵博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
